823e89d2d6508f4d5a0c92fc492d389566c123dd
PedroJCardoso <pedrojoaninicardoso@gmail.com>
2025-05-13 11:50:28 -0400

    Retornando emocoes em emotion detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -22,9 +22,14 @@ def emotion_detector(text_to_analyze):
         if max < emotion_score_dictionary[emotion]:
             dominant_emotion = emotion
             max = emotion_score_dictionary[emotion]
-    print(dominant_emotion)
-    print(emotion_score_dictionary)   
 
+    return {'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
     
     
     
